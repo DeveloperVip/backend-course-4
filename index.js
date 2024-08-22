a0b95eac30706a69b2ba88b233440527b022c766
DeverlopVip <hoanganhhung1403@gmail.com>
2024-08-22 10:11:06 +0700

    fix/cors

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ import historyRouter from "./routers/history.route.js";
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 const port = 8000;
 dotenv.config();
 //config cookie session
