bb70d13e9efc43e54b5767f924ebc0b700a201ed
DeveloperVip <130381016+DeveloperVip@users.noreply.github.com>
2024-09-10 10:15:11 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/user/auth/google/callback",
+      callbackURL: "https://backend-course-4.vercel.app/user/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
